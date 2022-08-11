a9eda6f63f7abcb0b936157865d1ddfafbbe3b9d
TrisnaWahyudi <wayantrisna79@gmail.com>
2022-08-11 18:29:55 +0800

    implemented delete functionality

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -6,7 +6,7 @@ export const getPosts = () => async (dispatch) => {
         const { data } = await api.fetchPosts();
         dispatch({ type: 'FETCH_ALL', payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 };
 
@@ -15,7 +15,7 @@ export const createPost = (post) => async (dispatch) => {
         const { data } = await api.createPost(post);
         dispatch({ type: 'CREATE', payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 };
 
@@ -25,6 +25,16 @@ export const updatePost = (id, post) => async (dispatch) => {
 
         dispatch({ type: 'UPDATE', payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
+    }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: 'DELETE', payload: id });
+    } catch (error) {
+        console.log(error);
     }
 };
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -4,10 +4,14 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 import useStyles from './styles';
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     return (
         <Card className={classes.card}>
@@ -27,8 +31,11 @@ const Post = ({ post, setCurrentId }) => {
                     {post.tags.map((tag) => `#${tag} `)}
                 </Typography>
             </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>
+                {post.title}
+            </Typography>
             <CardContent>
-                <Typography className={classes.title} variant="h5" gutterBottom>
+                <Typography variant="h5" gutterBottom>
                     {post.message}
                 </Typography>
             </CardContent>
@@ -38,7 +45,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
